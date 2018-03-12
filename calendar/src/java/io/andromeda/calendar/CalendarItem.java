e0f8caad59ddf8f03e529cdee7fdfce0d751b8a3
Alexander Brandt <abrandt@gmv-insyen.com>
2018-03-12 11:20:36 +0100

    Added calendar.

---
+++ calendar/src/java/io/andromeda/calendar/CalendarItem.java
@@ -0,0 +1,35 @@
+package io.andromeda.calendar;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class CalendarItem {
+    private static final ZoneId utcZone = ZoneId.of("Europe/Berlin");
+    private Calendar calendar;
+    private String name;
+    private ZonedDateTime startTime;
+    private ZonedDateTime endTime;
+    private ZonedDateTime lastModified;
+    private String location;
+    private String description;
+
+    public CalendarItem(Calendar calendar, String name, ZonedDateTime startTime, ZonedDateTime endTime,
+                        ZonedDateTime lastModified, String location, String description) {
+        this.calendar = calendar;
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.lastModified = lastModified;
+        this.location = location;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + calendar.getName() + "] " + name + "; "
+                + startTime.withZoneSameInstant(utcZone).format(Calendar.outputDateTimeFormatter) + "; "
+                + endTime.withZoneSameInstant(utcZone).format(Calendar.outputDateTimeFormatter) + "; "
+                + location + "; " + description + "; "
+                + lastModified.withZoneSameInstant(utcZone).format(Calendar.outputDateTimeFormatter);
+    }
+}
